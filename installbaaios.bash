0edca8d26579616d20eff58012c9288933634595
wahlmanj <wahlman.j@gmail.com>
2015-08-03 08:12:42 -0500

    Create installbaaios.bash

---
+++ installbaaios.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Update sources
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install cydia
+apt-get -y install wget
+
+# Trash PlexConnect Folder if exists to avoid database errors
+if [ -s /Applications/PlexConnect ]
+then
+rm -rf /Applications/PlexConnect
+fi
+# Clone Theme
+cd /Applications
+git clone git://github.com/iBaa/PlexConnect.git
+# Create Certs
+cd /Applications/PlexConnect
+openssl req -new -nodes -newkey rsa:2048 -outform pem -out ./assets/certificates/trailers.cer -keyout ./assets/certificates/trailers.key -x509 -days 3650 -subj "/C=US/CN=trailers.apple.com"
+cat ./assets/certificates/trailers.cer ./assets/certificates/trailers.key >> ./assets/certificates/trailers.pem
+# install requirements from atvjailbreak github if neeeded
+cd /Applications/atvjailbreak
+cp PlexConnect.py /Applications/PlexConnect
+cp PlexConnect.bash /Applications/PlexConnect/support/aTV_jailbreak
+if [ -f /usr/bin/python2.7 ];
+then
+   echo "Python already installed"
+else
+  dpkg -i python_2.7.3-3_iphoneos-arm.deb
+fi
+# install autoupdate plist
+cp update.bash /usr/bin
+cp updatebash.bash /usr/bin
+chmod +x /usr/bin/update.bash
+chmod +x /usr/bin/updatebash.bash
+cp com.plex.plexconnect.auto.plist /Library/LaunchDaemons
+chown root /Library/LaunchDaemons/com.plex.plexconnect.auto.plist
+chmod 644 /Library/LaunchDaemons/com.plex.plexconnect.auto.plist
+launchctl load /Library/LaunchDaemons/com.plex.plexconnect.auto.plist
+# Install launchctl bash plist
+chmod +x /Applications/PlexConnect/support/aTV_jailbreak/install.bash
+/Applications/PlexConnect/support/aTV_jailbreak/install.bash
+sleep 3
+launchctl unload /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
+launchctl load /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
+echo "Point your aTV DNS to your ios Device and upload you cert from your ios device to complete the process, trailers is hijacked by default"
