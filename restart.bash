7a6ff591d94b01527d3b1685951ae9acf64e1f71
wahlmanj <wahlman.j@gmail.com>
2015-09-16 21:48:19 -0500

    output more info

--- restart.bash
+++ restart.bash
@@ -9,3 +9,8 @@ sleep 2
 ## display the running status of PlexConnect
 #./PlexConnect_daemon.bash status
 launchctl list | grep com.plex.plexconnect.bash
+
+## Display PlexConnect log
+FILE="/Applications/PlexConnect/PlexConnect.log"
+echo "*** File - $FILE contents ***"
+cat $FILE
